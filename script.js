81c37a091dd4f7978909646f3f7c4be40530bbf8
Redcoder807 <gearonhaynes@gmail.com>
2022-03-01 10:08:16 -0600

    Pre-Bonus done

---
+++ script.js
@@ -0,0 +1,30 @@
+for (i = 1; i <= 7; i++){
+    console.log(i);
+}
+
+console.log(`========================`);
+
+for (x = 5; x <= 25; x += 4){
+    console.log(x);
+}
+
+const wizards = [
+    `Harry Potter`,
+    `Hermione Granger`,
+    `Ron Weasley`
+];
+
+for (char of wizards){
+    console.log(char);
+}
+
+console.log(`===================`);
+
+let harryPotterMovies = [0]
+
+while (harryPotterMovies < 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
+
+console.log(harryPotterMovies);
